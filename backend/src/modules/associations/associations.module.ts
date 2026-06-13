110f4975cc77350645f9f5048d1fd40702b608c4
Roger Bentcha <remirjunior1999@gmail.com>
2026-06-13 14:24:59 +0200

    fix import

--- backend/src/modules/associations/associations.module.ts
+++ backend/src/modules/associations/associations.module.ts
@@ -1,8 +1,8 @@
 import { Module } from '@nestjs/common';
 
+import { AuthModule } from '../auth/auth.module';
 import { AssociationsController } from './associations.controller';
 import { AssociationsService } from './associations.service';
-import { AuthModule } from '../auth/auth.module';
 
 @Module({
   imports: [AuthModule],

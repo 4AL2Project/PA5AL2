a212d8303c746d8c342135cffd17d97b143ff6f0
Roger Bentcha <remirjunior1999@gmail.com>
2026-06-13 14:24:59 +0200

    fix import

--- backend/src/modules/donations/donations.module.ts
+++ backend/src/modules/donations/donations.module.ts
@@ -1,10 +1,10 @@
 // Roger — v1.0
 import { Module } from '@nestjs/common';
 
+import { AuthModule } from '../auth/auth.module';
 import { CerfaService } from './cerfa.service';
 import { DonationsController } from './donations.controller';
 import { DonationsService } from './donations.service';
-import { AuthModule } from '../auth/auth.module';
 
 @Module({
   imports: [AuthModule],
